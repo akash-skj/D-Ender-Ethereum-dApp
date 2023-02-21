ac87b2785c6c3fcd2329ece29cb92493adc50182
Akash <akashsuresh.sj@gmail.com>
2023-02-21 20:50:26 +0530

    updated contract

--- src/contracts/dender.sol
+++ src/contracts/dender.sol
@@ -6,7 +6,7 @@ contract dender{
     struct tdr {
         uint id;
         string title;
-        //address mngr;
+        address mngr;
         string desc;
         uint startTime;
         uint endTime;
@@ -18,7 +18,7 @@ contract dender{
     struct bidder {
         string bidderName;
         bool bidded;
-        uint bidAmt;
+        mapping(uint => uint) bidAmt;
     }
 
     mapping(uint => tdr) public tdrs;
@@ -42,28 +42,70 @@ contract dender{
     public
     onlyOfficial
     {
-        tdrs[tdrCounts] = tdr(tdrCounts,_title,_description,_bidO,_bidC,0,0,0x0000000000000000000000000000000000000000);
+        tdrs[tdrCounts] = tdr(tdrCounts,_title,msg.sender,_description,_bidO,_bidC,0,0,0x0000000000000000000000000000000000000000);
         tdrCounts++;
     }
 
-   function bid (uint _amt, uint _tdrID, string memory _name)
+   function bid (uint _tdrID, string memory _name)
    public
    payable
    {
-       require(tdrs[_tdrID].startTime<block.timestamp, "Bid has not started.");
-       require(tdrs[_tdrID].endTime>block.timestamp, "Bid has ended.");
+       require(tdrs[_tdrID].startTime < block.timestamp, "Bid has not started.");
+       require(tdrs[_tdrID].endTime > block.timestamp, "Bid has ended.");
        if(bidders[msg.sender].bidded){
-           amt=_amt+bidders[msg.sender].bidAmt;
+           amt=msg.value+bidders[msg.sender].bidAmt[_tdrID];
        }else{
-           amt=_amt;
+           amt=msg.value;
        }
+       
        require(amt>tdrs[_tdrID].maxBid, "Bid is lower than the current bid.");
-       bidders[msg.sender] = bidder(_name, true, amt);
-       tdrs[_tdrID].accBal+=amt;
-       if(bidders[msg.sender].bidAmt>tdrs[_tdrID].maxBid){
+       bidder storage b = bidders[msg.sender];
+       b.bidderName = _name;
+       b.bidded = true;
+       b.bidAmt[_tdrID] = amt;
+       tdrs[_tdrID].accBal += amt;
+       if(bidders[msg.sender].bidAmt[_tdrID] > tdrs[_tdrID].maxBid){
            tdrs[_tdrID].maxBid = amt;
            tdrs[_tdrID].highestBidder = msg.sender;
        }
+
+       if(tdrs[_tdrID].endTime-block.timestamp < 600){
+           tdrs[_tdrID].endTime+= 600-(tdrs[_tdrID].endTime-block.timestamp);
+       }
+   }
+
+   function highestBidOfTdr (uint _tdrID)
+   public
+   view
+   returns(uint)
+   {
+       return tdrs[_tdrID].maxBid;
+   }
+
+   function highestBidderOfTdr (uint _tdrID)
+   public
+   view
+   returns(address)
+   {
+       return tdrs[_tdrID].highestBidder;
+   }
+
+   function testbids (uint _id) 
+   public
+   view
+   returns(uint, string memory)
+   {
+       return (bidders[msg.sender].bidAmt[_id], tdrs[_id].title);
+   }
+
+   function withdrawFunds (uint _tdrID) 
+   public
+   payable
+   {
+        require(tdrs[_tdrID].endTime < block.timestamp, "Bid has not ended.");
+        require(bidders[msg.sender].bidAmt[_tdrID] > 0, "You have not placed any bids.");
+        bool sent = payable(msg.sender).send(bidders[msg.sender].bidAmt[_tdrID]);
+        require(sent,"Error");
    }
 
 }
