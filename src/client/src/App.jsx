a93c468f5959e5ed9c2ea18d6c8f9cddc7082df2
Akash <akashsuresh.sj@gmail.com>
2023-02-27 00:54:34 +0530

    Installed daisyui plugin for tailwindcss

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App= () => {
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen flex-col items-center justify-center">
+      {/* The button to open modal */}
+      <label htmlFor="my-modal-3" className="btn w-max">open modal</label>
+
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative">
+        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+          <div className="form-control w-full max-w-xs flex flex-col items-center">
+            <label className="label">
+              <span className="label-text">What is your name?</span>
+              <span className="label-text-alt">Top Right label</span>
+            </label>
+            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+  </div>
   )
 }
 
