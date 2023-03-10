ca0caec3eb9cc7a53a6d3245d4019c00bbeb8a94
Akash <akashsuresh.sj@gmail.com>
2023-03-11 00:38:12 +0530

    Restructured TenderCard component

--- src/client/src/components/TenderCards.jsx
+++ src/client/src/components/TenderCards.jsx
@@ -10,13 +10,13 @@ const TenderCards = (props) => {
 
                 </div>
                 :
-                <div className="bg-base-300 h-[250px] w-[350px] rounded-lg p-4 my-7 mx-4 border-solid border-primary border-t-[1px] border-b-[1px] flex flex-col text-center ">
+                <div className="bg-base-300 h-[500px] w-[315px] rounded-lg p-4 my-7 mx-4 border-solid border-primary border-t-[1px] border-b-[1px] flex flex-col text-center ">
                 <div className="text-3xl">
                     {props.title}
                 </div>
                 <div className="flex flex-col h-full justify-center text-ellipsis overflow-hidden">
-                    {(props.desc.length)>77 && ( <div>{props.desc.slice(0,77)} . . .</div> )}
-                    {(props.desc.length)<77 && ( <div>{props.desc}</div> )}
+                    {(props.desc.length)>250 && ( <div>{props.desc.slice(0,77)} . . .</div> )}
+                    {(props.desc.length)<250 && ( <div>{props.desc}</div> )}
                 </div>
                 <div>
                     <button className="btn bg-base-100 m-3">Open</button>
