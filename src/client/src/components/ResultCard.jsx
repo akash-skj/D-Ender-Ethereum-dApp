1bf6bec7c33e2e7f756e09446144f5963bb12b3b
Akash <akashsuresh.sj@gmail.com>
2023-03-22 23:50:13 +0530

    Initialize Result card component

---
+++ src/client/src/components/ResultCard.jsx
@@ -0,0 +1,17 @@
+const ResultCard = () => {
+    return (
+        <div className="flex flex-col bg-base-300 h-full ml-2 rounded-lg p-5 bg-opacity-80">
+            
+            <div>
+                <h1 className="font-bold text-4xl">Result Card</h1>
+            </div>
+
+            <div className="flex ">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ResultCard;
